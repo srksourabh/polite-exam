9aad33bf18ee7a4970af69483319213323a810a8
Sourabh Bhaumik <srksourabh@gmail.com>
2025-12-01 09:33:17 +0530

    Clean up unnecessary files - removed docs, build files, presentations, and Android-specific files

--- android/app/src/main/java/com/politecoaching/exam/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.politecoaching.exam;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- build-app.bat
+++
@@ -1,2 +0,0 @@
-cd "C:\Users\soura\Dropbox\AI\Projects\Polite_exam\polite-exam"
-npx @bubblewrap/cli build
